01b5cb8d06ab5a3ac790ae55c87c609b1182b0fb
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-04-13 05:38:51 +0530

    feat: expose pick function outside

--- lua/pickme.lua
+++ lua/pickme.lua
@@ -1,6 +1,14 @@
-return {
-    setup = function(opts)
-        require('pickme.config').setup(opts)
-        require('pickme.commands').setup()
-    end,
-}
+local main = require('pickme.main')
+
+local M = {}
+
+M.setup = function(opts)
+    require('pickme.config').setup(opts)
+    require('pickme.commands').setup()
+end
+
+M.pick = function(command, opts)
+    return main.pick(command, opts)
+end
+
+return M
